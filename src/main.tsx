81855b9352709be1170af46b747312e22d1e677a
rtemplo <30515414+rtemplo@users.noreply.github.com>
2026-06-12 15:59:52 -0400

    restore strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,20 @@ import { TaskFormProvider } from "./contexts/TaskFormContext";
 import { TaskManagerProvider } from "./contexts/TaskManagerContext";
 
 import "./style.css";
+import { StrictMode } from "react";
 import { TaskFilterProvider } from "./contexts/TaskManagerFilterContext";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   createRoot(rootElement).render(
-    // <StrictMode>
-    //   <BrowserRouter>
-    //     <App />
-    //   </BrowserRouter>
-    // </StrictMode>
-    <TaskFilterProvider>
-      <TaskManagerProvider>
-        <TaskFormProvider>
-          <TaskManager />
-        </TaskFormProvider>
-      </TaskManagerProvider>
-    </TaskFilterProvider>
+    <StrictMode>
+      <TaskFilterProvider>
+        <TaskManagerProvider>
+          <TaskFormProvider>
+            <TaskManager />
+          </TaskFormProvider>
+        </TaskManagerProvider>
+      </TaskFilterProvider>
+    </StrictMode>
   );
 }
